86e79a466710ee22e4b3ebce9a520f0fce9ae6a8
Hauke Sommerfeld <development@haukesomm.de>
2023-04-16 00:19:28 +0200

    Downgrade gradle and fix build errors

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,8 +3,6 @@ plugins {
     id("com.google.devtools.ksp")
 }
 
-val fritz2version = rootProject.ext["fritz2version"]
-
 kotlin {
     jvm()
     js(IR) {
@@ -12,31 +10,6 @@ kotlin {
     }
 
     sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation("dev.fritz2:core:$fritz2version")
-            }
-        }
+        val commonMain by getting
     }
 }
-
-/**
- * KSP support - start
- */
-dependencies {
-    add("kspCommonMainMetadata", "dev.fritz2:lenses-annotation-processor:$fritz2version")
-}
-kotlin.sourceSets.commonMain { kotlin.srcDir("build/generated/ksp/metadata/commonMain/kotlin") }
-
-// Fixes webpack-cli incompatibility by pinning the newest version.
-// https://youtrack.jetbrains.com/issue/KTIJ-22030
-rootProject.extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
-    versions.webpackCli.version = "4.10.0"
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.dsl.KotlinCompile<*>>().all {
-    if (name != "kspCommonMainKotlinMetadata") dependsOn("kspCommonMainKotlinMetadata")
-}
-/**
- * KSP support - end
- */
