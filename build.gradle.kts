381d16f9e67744e41bdcfd0b88817f92688bc7ec
Hauke Sommerfeld <development@haukesomm.de>
2023-09-24 16:28:06 +0200

    Set version to 0.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ allprojects {
 
 subprojects {
     group = "de.haukesomm.sokoban"
-    version = "0.1"
+    version = "0.2-SNAPSHOT"
 
     tasks.withType<KotlinCompile> {
         kotlinOptions {
